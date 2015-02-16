0da6c08db9efff9dcbf95861fbb5f9227443bd47
David Ahlbeck <david@ahlbeck.nu>
2015-02-16 19:47:31 +0100

    project setup

---
+++ src/main/groovy/SomeCycles.groovy
@@ -0,0 +1,14 @@
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+/**
+ * Created by dave on 16/02/15.
+ */
+class SomeCycles implements Plugin<Project> {
+    @Override
+    void apply(Project target) {
+        target.tasks.create(name:'SomeCycles',dependsOn: ['compileJava']) << {
+            println "Dodge this!"
+        }
+    }
+}
---
+++ src/main/groovy/SomeCyclesExtension.groovy
@@ -0,0 +1,5 @@
+/**
+ * Created by dave on 16/02/15.
+ */
+class SomeCyclesExtension {
+}
